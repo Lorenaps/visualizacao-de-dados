f9b0097022c322bcfda27e3268ffc5c8209b99c4
Lorenaps <lorena.santpe@gmail.com>
2018-10-29 23:25:48 -0300

    Adicionando dados agrupados para analise do preço do Big Mac

--- dados/transformacao_lab1.R
+++ dados/transformacao_lab1.R
@@ -1,15 +1,13 @@
 library(jsonlite)
-library(RJSONIO)
 library(dplyr)
 library(magrittr)
 library(readr)
 
 
-
-authors = read.csv("/home/lsp/Projetos/dados/vis_lab1/authors.csv", header = T)
-conferences = read.csv("/home/lsp/Projetos/dados/vis_lab1/conferences.csv", header = T)
-papers = read.csv("/home/lsp/Projetos/dados/vis_lab1/papers.csv", header = T)
-ethnicity = read.csv("/home/lsp/Projetos/dados/vis_lab1/ethnicity.csv", header = T)
+authors = read.csv("/home/lsp/dados/vis_lab1/authors.csv", header = T)
+conferences = read.csv("/home/lsp/dados/vis_lab1/conferences.csv", header = T)
+papers = read.csv("/home/lsp/dados/vis_lab1/papers.csv", header = T)
+ethnicity = read.csv("/home/lsp/dados/vis_lab1/ethnicity.csv", header = T)
 
 author_ethnicity = merge(authors, ethnicity, by="full_name")
 
@@ -17,8 +15,34 @@ author_ethnicity_paper = merge(author_ethnicity, papers, by="paper_key")
 
 dataset = merge(author_ethnicity_paper, conferences, by="conf_key")
 
+# Selecionando papers publicados depois de 2009
 dataset = dataset %>% dplyr::select(conf_key, year, paper_key, full_name, et_0, 
                                     et_1, et_2, gender, position, publisher, 
-                                    cs, de, se, th) %>% filter(year > 2015)
+                                    cs, de, se, th) %>% filter(year > 2009) 
 
-write.csv(dataset, file = "/home/lsp/Projetos/dados/vis_lab1/dataset_2015.csv")
+# Verificando margem de valores dos dados
+dataset %>% group_by(gender) %>% summarise(length(gender))
+dataset %>% group_by(et_2) %>% summarise(length(et_2))
+dataset %>% group_by(et_1) %>% summarise(length(et_1))
+dataset %>% group_by(et_0) %>% summarise(length(et_0))
+
+# Tratando valores não informados para o gênero
+dataset = mutate(teste, gender = case_when(gender == '-' ~ "Não Informado", 
+                                          gender == 'F' ~ "Feminino", 
+                                          gender == 'M' ~ "Masculino"))
+
+# Tratando valores não informados para o etnia 2
+dataset = mutate(dataset, et_2 = ifelse(et_2 == "\\N", "Não Informado", as.character(et_2)))
+
+# Verificando margem de valores dos dados após tratamento
+dataset %>% group_by(gender) %>% summarise(length(gender))
+dataset %>% group_by(et_2) %>% summarise(length(et_2))
+dataset %>% group_by(et_1) %>% summarise(length(et_1))
+dataset %>% group_by(et_0) %>% summarise(length(et_0))
+
+teste = dataset
+
+te = dataset %>% group_by(gender, publisher, year, et_1) %>% tally()
+
+write.csv(te, file = "Projetos/Python/visualizacao-de-dados/dados/publisher_agregado.csv")
+
